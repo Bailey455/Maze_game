222ec9c9fa2bb8c7f318c631507867e763e796a5
Bailey455 <90922108+Bailey455@users.noreply.github.com>
2023-01-06 13:58:11 -0500

    player_only_runs_through_z_walls
    need to fix ability for player to go through z walls

--- maze_game/Assets/playerMovement.cs
+++ maze_game/Assets/playerMovement.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    public float movementSpeed = 10.0f;
+    public float movementSpeed = 100.0f;
     public bool isSprinting = false;
+
+    private Rigidbody rb;
+
+    public void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
 
     public void FixedUpdate()
     {
@@ -16,6 +23,9 @@ public class playerMovement : MonoBehaviour
         movementDirection.Normalize();
 
         transform.Translate(movementDirection * Time.deltaTime * movementSpeed);
+
+        //works for left to right movement but not up and down
+        //rb.MovePosition(transform.position + movementDirection * movementSpeed * Time.deltaTime);
 
         if (Input.GetKey(KeyCode.LeftShift))
         {
@@ -34,5 +44,6 @@ public class playerMovement : MonoBehaviour
         {
             movementSpeed = 3.0f;
         }
+        
     }
 }
