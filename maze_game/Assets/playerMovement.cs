109a5831c6aebc39d0f5882fe53badf862e08558
Bailey455 <90922108+Bailey455@users.noreply.github.com>
2023-01-06 13:07:51 -0500

    drift_fix

--- maze_game/Assets/playerMovement.cs
+++ maze_game/Assets/playerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    public float movementSpeed = 5.0f;
+    public float movementSpeed = 10.0f;
     public bool isSprinting = false;
 
     public void FixedUpdate()
