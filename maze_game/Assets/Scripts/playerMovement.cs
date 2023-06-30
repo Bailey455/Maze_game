6169a9a70a2d3d680017032ee17fbd01ed29835b
Bailey455 <90922108+Bailey455@users.noreply.github.com>
2023-06-30 14:40:53 -0400

    update_player_move

--- maze_game/Assets/Scripts/playerMovement.cs
+++ maze_game/Assets/Scripts/playerMovement.cs
@@ -4,34 +4,38 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    public float movementSpeed = 0.0f;
+    public float movementSpeed = 10.0f;
     public bool isSprinting = false;
+    private float gravity = -20f;
 
     private Rigidbody rb;
     private Animator animator;
+    CharacterController cb; 
 
     public void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
         animator = gameObject.GetComponent<Animator>();
+        cb = gameObject.GetComponent<CharacterController>();
     }
 
     public void FixedUpdate()
     {
-        // fancy movement script
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-        movementDirection.Normalize();
+    // fancy movement script
+    float horizontalInput = Input.GetAxis("Horizontal");
+    float verticalInput = Input.GetAxis("Vertical");
+    Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+    movementDirection.Normalize();
 
-        transform.Translate(movementDirection * Time.deltaTime * movementSpeed, Space.World);
+    transform.Translate(movementDirection * Time.deltaTime * movementSpeed, Space.World);
 
-        animator.SetFloat("speed", movementSpeed);
+    animator.SetFloat("speed", movementSpeed);
 
-        if (movementDirection != Vector3.zero)
-        {
-            transform.forward = movementDirection;
-        }
+    if (movementDirection != Vector3.zero)
+    {
+        transform.forward = movementDirection;
+    }   
+        
 
         /*if (Input.GetKey(KeyCode.LeftShift))
         {
